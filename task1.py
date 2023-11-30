169d3a94efcd40259ddbaf1b144a0df96eaf5255
Volodya <volodya.atoyan@list.ru>
2023-11-30 18:05:23 +0300

    add task1

---
+++ task1.py
@@ -0,0 +1,15 @@
+def short_list_imperative(numbers):
+    n = len(numbers)
+    
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if numbers[j] < numbers[j+1]:
+                # меняем элементы местами, если текущий больше следующего
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+                
+    return numbers
+
+# Пример использования
+numbers = [64, 34, 25, 12, 22, 11, 90]
+sorted_numbers = short_list_imperative(numbers)
+print("Отсортированный список в порядке убывания:", sorted_numbers)
